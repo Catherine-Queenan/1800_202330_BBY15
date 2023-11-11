b13e0dcac3c50f45b48504ee75f4c3029a25863b
Joyce Huang <whuang152@my.bcit.ca>
2023-11-10 21:28:01 -0800

    add dogprofile.html dogprofile.js df-style.css that make a create pup profile form and link the pup profile to the user in firestore databse. Modify the map.js to add read more information in the popup section.

---
+++ scripts/dogprofile.js
@@ -0,0 +1,65 @@
+function addDogProfiles() {
+    var name = document.getElementById("name").value;
+    var breed = document.getElementById("breed").value;
+    var gender = document.getElementById("gender").value;
+    var age = document.getElementById("age").value;
+    var status = document.getElementById("status").value;
+    var about = document.getElementById("about").value;
+    var birthday = document.getElementById("birthday").value;
+    var userId = firebase.auth().currentUser.uid;
+
+    // Get a reference to the dog-profiles collection
+    const dogProfilesCollection = db.collection("dog-profiles");
+
+    // Check if the dog-profiles collection exists
+    dogProfilesCollection.get()
+        .then(function (querySnapshot) {
+            if (querySnapshot.empty) {
+                // Create the dog-profiles collection if it doesn't exist
+                db.collection("dog-profiles").doc('placeholder').set({}); // Use 'set' to create the collection
+            }
+
+            // add user details to database
+            dogProfilesCollection.add({
+                name: name,
+                breed: breed,
+                gender: gender,
+                age: age,
+                status: status, 
+                about: about, 
+                birthday: birthday,
+                ownerId: userId
+            }).then(function (docRef) {
+                console.log("Dogs details saved successfully with ID: ", docRef.id);
+
+                // add dog profile ID to user's document
+                db.collection("users").doc(userId).update({
+                    dogs: firebase.firestore.FieldValue.arrayUnion(docRef.id)
+                })
+                    .then(function () {
+                        console.log("Dog profile ID added to user's document successfully");
+                    })
+                    .catch(function (error) {
+                        console.error("Error adding dog profile ID to user's document: ", error);
+                        alert("Error adding dog profile ID to user's document!");
+                    });
+
+                    // clear form input fields
+                    document.getElementById("name").value = "";
+                    document.getElementById("breed").value = "";
+                    document.getElementById("gender").value = "";
+                    document.getElementById("age").value = "";
+                    document.getElementById("status").value = "";
+                    document.getElementById("about").value = "";
+                    document.getElementById("birthday").value = "";
+                })
+                .catch(function (error) {
+                    console.error("Error saving dog profiles: ", error);
+                    alert("Error saving dog profile!");
+                });
+        })
+        .catch(function (error) {
+            console.error("Error checking dog-profiles collection: ", error);
+            alert("Error checking dog-profiles collection!");
+        });
+    }
