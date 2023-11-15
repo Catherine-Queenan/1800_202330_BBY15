f1abaa7405ff78562a1151eb5ad8207e1a71665a
Joyce Huang <whuang152@my.bcit.ca>
2023-11-15 13:40:06 -0800

    change the pupprofile.* file name to pupprofileForm. Link all the pages about pupprofiles. Main pupprofile page has a plus button that can create a new pup profile. Inside the main pupprofile page which is pupprofileList.html has cards that display the simple pupprofile that the user created. By clicking each pupprofile card, it can link to that specific pupprofile.

--- scripts/eachPupprofile.js
+++ scripts/eachPupprofile.js
@@ -1,8 +1,8 @@
 // Retrieve user id
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     var userId = user.uid;
-    
+
     // Retrieve dogs array for the user
     var userRef = db.collection("users").doc(userId);
     userRef.get().then((doc) => {
@@ -13,21 +13,36 @@ firebase.auth().onAuthStateChanged(function(user) {
           var dogRef = db.collection("dog-profiles").doc(dogId);
           dogRef.get().then((doc) => {
             if (doc.exists) {
-              var data = doc.data();
-              
-              // Update HTML elements with the retrieved data
-              $('.pupName').text(data.name);
-              $('#pupBreed').text(data.breed);
-              $('#pupBday').text(data.birthday);
-              $('#pupAge').text(data.age);
-              $('#pupGender').text(data.gender);
-              $('#pupStatus').text(data.status);
-              $('#pupAbout').text(data.about);
-              // $('#imgProfile').attr('src', data.profilePicture);
+              var urlParams = new URLSearchParams(window.location.search);
+              var pupprofileId = urlParams.get('id');
+              // Use the pupprofileId to fetch the specific dog profile data from the Firebase database
+              // and display it on the profile page
+              db.collection("dog-profiles").doc(pupprofileId).get()
+                .then(function (doc) {
+                  if (doc.exists) {
+                    // here get the specific dog-profile and assign it to data
+                    var data = doc.data();
+
+                    // Update HTML elements with the retrieved data
+                    $('.pupName').text(data.name);
+                    $('#pupBreed').text(data.breed);
+                    $('#pupBday').text(data.birthday);
+                    $('#pupAge').text(data.age);
+                    $('#pupGender').text(data.gender);
+                    $('#pupStatus').text(data.status);
+                    $('#pupAbout').text(data.about);
+                    // $('#imgProfile').attr('src', data.profilePicture);
+
+                  } else {
+                    console.log("No such document!");
+                  }
+                });
             } else {
               console.log("No such document!");
             }
-        });
+          }).catch((error) => {
+            console.log("Error getting document:", error);
+          });
         });
       } else {
         console.log("No such document!");
--- scripts/pupprofileList.js
+++ scripts/pupprofileList.js
@@ -1,9 +1,58 @@
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        var userId = user.uid;
+
+        // Retrieve dogs array for the user
+        var userRef = db.collection("users").doc(userId);
+        userRef.get().then((doc) => {
+            if (doc.exists) {
+                var dogsArray = doc.data().dogs;
+                var cardContainer = document.getElementById('card-container');
+
+                dogsArray.forEach((dogId) => {
+                    // Retrieve data for each dog profile
+                    var dogRef = db.collection("dog-profiles").doc(dogId);
+                    dogRef.get().then((doc) => {
+                        if (doc.exists) {
+                            var data = doc.data();
+
+                            // Create card template using jQuery UI draggable
+                            var cardTemplate = `
+                                <div class="col-md-3" pup-card" onclick="viewProfile('${dogId}')">
+                                    <div class="card-sub" style="cursor: pointer";>
+                                        <img class="card-img-top img-fluid" src="${data.imageURL}" alt="Pup profile img">
+                                        <div class="card-block">
+                                            <h4 class="card-title">${data.name}</h4>
+                                            <!-- Add other relevant dog profile information to the card template -->
+                                        </div>
+                                    </div>
+                                </div>
+                            `;
+
+                            // Append the card template to the card container
+                            cardContainer.innerHTML += cardTemplate;
+                        }
+                    });
+                });
+            }
+        }).catch((error) => {
+            console.log("Error getting user document:", error);
+        });
+    }
+});
+
+function viewProfile(dogId) {
+     // Redirect to the specific pupprofile.html page with the corresponding dog ID
+  window.location.href = `eachPupprofile.html?id=${dogId}`;
+}
+
 $(document).ready(function () {
-    $("#sortable").sortable();
-    $("#sortable").disableSelection();
+    $(".pup-card").draggable();
+    $("#card-container").sortable();
+    $("#card-container").disableSelection();
 });
 
 const createbtn = document.getElementById("btn-createPupprofile");
 createbtn.addEventListener("click", () => {
-    window.location.href = 'pupprofile.html';
+    window.location.href = 'pupprofileForm.html';
 });
